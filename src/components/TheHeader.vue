c4908176857a58bd8aa8cba2d9fecc4b603ed24a
paulperrone <pperronecareer@gmail.com>
2021-09-22 13:48:15 -0700

    build for vue-version

--- src/components/TheHeader.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <header class="bg-white">
-    <nav class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
-      <div class="w-full pt-6 flex items-center justify-center">
-        <p>paul perrone</p>
-      </div>
-    </nav>
-  </header>
-</template>
